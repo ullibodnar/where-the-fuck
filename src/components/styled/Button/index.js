ab55959960dea255e221fc06f381fd7fbbfa0d4b
Ulli Bodnar <ullibodnar@gmail.com>
2018-08-12 15:30:11 +1200

    fix styling and add queries

--- src/components/styled/Button/index.js
+++ src/components/styled/Button/index.js
@@ -14,19 +14,18 @@ const Button = styled.button`
     color: white;
   }
 
-  @media only screen and (min-height: 700px) {
-    @media only screen and (min-width: 768px) {
-      padding: 0 0.4em;
-      font-size: 2.8em;
-    }
-    @media only screen and (min-width: 1000px) {
-      padding: 0 0.4em;
-      font-size: 3em;
+  @media only screen and (min-height: 800px) {
+    padding: 0 0.4em;
+    font-size: 2.4em;
+    @media only screen and (min-height: 870px) {
+      @media only screen and (min-width: 1000px) {
+        padding: 0 0.4em;
+        font-size: 3em;
+      }
     }
   }
-
-  
 `
+
 Button.displayName = 'StyledButton'
 
 export default Button
